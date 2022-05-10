e7bd3eda4197df83bb2fe9656770fb994294b937
Stan Smoczyk <stansmoczyk@gmail.com>
2022-05-10 15:31:41 -0400

    Passed all but number string have to revisit later

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(...arr) {
+let min = Math.min(...arr);
+let max = Math.max(...arr);
 
+let sum = 0;
+if (isNaN(max) || isNaN(min)){
+    return 'ERROR'
+}
+else if(min >0 && max > 0){
+for (let i = min; i <= max; i++){
+    sum += i;
+}
+}else {
+    return 'ERROR'
+}
+return sum;
 };
 
 // Do not edit below this line
