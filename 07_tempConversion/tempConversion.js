063dec4e05972f258981ede393e7592b9caed171
Stan Smoczyk <stansmoczyk@gmail.com>
2022-05-11 10:56:47 -0400

    Completed moduals

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(...arr) {
+let celsius = (arr - 32) / 1.8;
+return Math.round(celsius * 10) /10; 
 };
 
-const ctof = function() {
-
+const ctof = function(...arr) {
+let fehrenheit = (arr * 1.8) + 32;
+return Math.round(fehrenheit * 10) / 10;
 };
 
 // Do not edit below this line
